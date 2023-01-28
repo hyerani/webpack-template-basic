5735613b22d5ad593ef2b618f1c9658d7db8ff4f
hyerani <hran9404@naver.com>
2023-01-28 20:38:40 +0900

    babel presets 수정

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: ["@babel/preset-env"], // js 기능을 한번에 지원
+  presets: ["@babel/preset-env"], // js 기능을 한번에 지원
   plugins: [
     ["@babel/plugin-transform-runtime"], // 비동기 처리를 위한 플러그인
   ],
